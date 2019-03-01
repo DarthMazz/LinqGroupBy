c06071be6d4425d8a4c8248df3f1a35d7b27919c
yo4taka <yo4taka.ma2moto@gmail.com>
2019-03-01 21:42:56 +0900

    Studyプロジェクトを追加

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Study01
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
